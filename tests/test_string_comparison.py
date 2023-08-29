502dc68c08ead5b6f82d6e917097d71d676d4d26
MatheusHRV <matheush49@yahoo.com>
2023-08-29 06:26:00 -0300

    pytest on github action assert true

--- tests/test_string_comparison.py
+++ tests/test_string_comparison.py
@@ -1,6 +1,6 @@
 
 def test_string_comparison():
     string_a = "Hello, World!"
-    string_b = "Hello, World"
+    string_b = "Hello, World!"
 
     assert string_a == string_b, "Strings are not the same"
